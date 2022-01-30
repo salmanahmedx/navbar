2b9a3658a38522073d359b14e8bdd4278aa63ece
salmanahmedxyz <salmanahmedx@hotmail.com>
2022-01-30 06:20:48 +0600

    final commit

--- app.js
+++ app.js
@@ -3,3 +3,22 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector(".links");
+
+//if-else method
+
+// navToggle.addEventListener("click", function () {
+//     if (links.classList.contains("show-links")) {
+//         links.classList.remove("show-links")
+//     } else {
+//         links.classList.add("show-links")
+//     }
+// })
+
+//toggle method
+
+navToggle.addEventListener("click", function () {
+    links.classList.toggle("show-links")
+})
